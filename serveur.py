d6f26ace348d2449fcaf5e02b3ffa1c15bb3edb9
apoleon33 <71031475+apoleon33@users.noreply.github.com>
2022-04-14 15:08:23 +0200

    redirection 3

--- serveur.py
+++ serveur.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def response():
+    return render_template('acceuil.html')
+
+@app.route("/jouer")
+def response2():
     return render_template('index.html')
 
 if __name__=="__main__":
